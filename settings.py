b17572b82f4e714dda5cf07def5e6be88093c6ba
Aleena <aleenakb9037@gmail.com>
2026-03-06 22:20:58 +0530

    Added GitHub Actions workflow for result and voting Django tests by aleena

--- settings.py
+++ settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'voting_app',
+    'voting_project.voting_app',
 ]
 
 MIDDLEWARE = [
